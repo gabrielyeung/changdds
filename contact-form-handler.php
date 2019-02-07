7fe13ebac59d0c83291e38dfe31fd442f8f97e58
gyeung <gyeung@expedia.com>
2019-02-07 14:49:28 -0800

    config+update email
    to update the email:
    1. goto _config.yml, change the email value there,
    2. also goto contact-form-handler.php, change the email value there

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -1,7 +1,7 @@
-<?php 
+<?php
 $errors = '';
 // $myemail = 'gabs1523@gmail.com';
-$myemail = 'quyenchangdds@gmail.com';
+$myemail = 'info@quyenchangdds.com';
 
 if(empty($_POST['name'])  ||
    empty($_POST['number']) ||
@@ -16,7 +16,7 @@ $email_address = $_POST['email'];
 $message = $_POST['message'];
 
 if (!preg_match(
-"/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i", 
+"/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i",
 $email_address))
 {
     $errors .= "\n Error: Invalid email address";
@@ -24,7 +24,7 @@ $email_address))
 
 if( empty($errors))
 {
-	$to = $myemail; 
+	$to = $myemail;
 	$email_subject = "Contact form submission: $name";
 	$email_body = "New Appointment Request. Here are the details -\n\n".
 	  "Name: $name\nNumber: $number\nEmail: $email_address\nMessage: $message";
@@ -33,9 +33,9 @@ if( empty($errors))
 
 	//redirect to the 'thank you' page
 	//header('Location: contact-form-thank-you.html');
-} 
+}
 ?>
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd"> 
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
 	<title>Contact form handler</title>
